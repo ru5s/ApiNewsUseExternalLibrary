9083939efbd1e3d520c32e65f301267a56f2a99f
Ruslan <hellperock@gmail.com>
2023-05-16 11:38:21 +0500

    fix issue with cocoa libraries

---
+++ ApiNewsUseExternalLibrary/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ApiNewsUseExternalLibrary
+//
+//  Created by Ruslan Ismailov on 01/12/22.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
